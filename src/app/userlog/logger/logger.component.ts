862e833ec17abfe9068428ff556d1c81607dea8d
Avneet <avneet12284@gmail.com>
2019-09-10 12:54:49 -0400

    forgot to unsubscribe - my bad - jut hit me now!!!

--- src/app/userlog/logger/logger.component.ts
+++ src/app/userlog/logger/logger.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, ChangeDetectionStrategy, Input, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/core-data/api.service';
 import { map } from 'rxjs/operators';
@@ -10,18 +10,23 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./logger.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoggerComponent implements OnInit {
+export class LoggerComponent implements OnInit, OnDestroy {
 
   type:string;
+  private loggerSubscription: Subscription
 
   constructor(private apiService: ApiService,route: ActivatedRoute){
     this.type = route.snapshot.queryParamMap.get('type');    
   }
 
   ngOnInit(){
-    this.apiService.getDataModelRefByType(this.type)
+    this.loggerSubscription=this.apiService.getDataModelRefByType(this.type)
                                      .pipe(map(obj => obj.dataModelValues))
                                      .subscribe(val => console.log(val));
+  }
+
+  ngOnDestroy(){
+    this.loggerSubscription.unsubscribe();
   }
     
 }
